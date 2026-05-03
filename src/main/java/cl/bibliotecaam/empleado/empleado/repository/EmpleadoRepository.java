8019f4a16e089b4ecd552b9d3d84640841d6bf6b
Alfonso Pizarro <ajpizarror@gmail.com>
2026-05-03 15:12:50 -0400

    repository

---
+++ src/main/java/cl/bibliotecaam/empleado/empleado/repository/EmpleadoRepository.java
@@ -0,0 +1,14 @@
+package cl.bibliotecaam.empleado.empleado.repository;
+
+import cl.bibliotecaam.empleado.empleado.model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+
+    List<Empleado> findAll();
+    List<Empleado> findByNumrun_emp(Long numrun);
+    List<Empleado> findByAppaterno_emp(String appaterno);
+    List<Empleado> findBySueldo(Long sueldo);
+}
