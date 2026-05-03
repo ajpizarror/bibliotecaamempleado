5b339d3992aee04ab219e90e58f63d465a82fb8a
Alfonso Pizarro <ajpizarror@gmail.com>
2026-05-03 14:56:05 -0400

    primer commit

---
+++ src/main/java/cl/bibliotecaam/empleado/empleado/model/Empleado.java
@@ -0,0 +1,45 @@
+package cl.bibliotecaam.empleado.empleado.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_empleado;
+
+    @Column(nullable = false, length = 8, unique = true)
+    private Long numrun_emp;
+
+    @Column(nullable = false, length = 1, unique = false)
+    private String dvrun_emp;
+
+    @Column(nullable = false, length = 30, unique = false)
+    private String pnombre_emp;
+
+    @Column(nullable = true, length = 30, unique = false)
+    private String snombre_emp;
+
+    @Column(nullable = false, length = 30, unique = false)
+    private String appaterno_emp;
+
+    @Column(nullable = false, length = 30, unique = false)
+    private String apmaterno_emp;
+
+    @Column(nullable = false)
+    private LocalDate fecha_contrato;
+
+    @Column(nullable = false, length = 7, unique = false)
+    private Long sueldo;
+
+
+}
