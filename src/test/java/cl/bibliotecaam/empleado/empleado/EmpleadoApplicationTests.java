e431d33f90ec0d2f9e0da7d077416868c901618e
Alfonso Pizarro <ajpizarror@gmail.com>
2026-05-03 14:45:57 -0400

    probando empleado commit

---
+++ src/test/java/cl/bibliotecaam/empleado/empleado/EmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bibliotecaam.empleado.empleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
