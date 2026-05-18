0d60c23006b1419d33ba51e9263edecebc5116b0
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-18 13:49:05 -0400

    Probando Seguridad - EN PROCESO

---
+++ src/main/java/cl/bibliotecaam/empleado/empleado/auth/AuthController.java
@@ -0,0 +1,25 @@
+package cl.bibliotecaam.empleado.empleado.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bibliotecaam/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping(value = "login")
+    public String login(){
+        return "Login from public endpoint";
+    }
+
+    @PostMapping(value = "register")
+    public String register()
+    {
+        return "Register from public endpoint";
+    }
+
+
+}
