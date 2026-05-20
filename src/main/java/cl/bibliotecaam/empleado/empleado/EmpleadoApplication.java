0704b02127864d17ed72d81d2bb51d030ff5d251
MAGAQ <panamasado41@gmail.com>
2026-05-19 23:20:50 -0400

    Aviso de utilidad en EmpleadoApplication - Empleado con nueva funcion por Snombre - Authenticator funcionando - MS listo para el ataque >:D

--- src/main/java/cl/bibliotecaam/empleado/empleado/EmpleadoApplication.java
+++ src/main/java/cl/bibliotecaam/empleado/empleado/EmpleadoApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+/*
+* * * * * * NOTA A FUTURO
+------> El Authenticator puede ser un MS aparte, a pesar de eso, vamos a dejarlo simple para
+* Que la logica indique que se esta autenticando principalmente al MS de Empleado.
+* En la evolucion futura, debemos separarlo y vincularlo con los MS que necesiten su token.
+ */
 public class EmpleadoApplication {
 
 	public static void main(String[] args) {
