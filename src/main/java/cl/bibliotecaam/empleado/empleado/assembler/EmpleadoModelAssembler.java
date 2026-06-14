a7d2e5db5230425d9b215d51e51038b4e11e9488
Alfonso Pizarro <ajpizarror@gmail.com>
2026-06-14 13:54:19 -0400

    falta swagger y hateoas

---
+++ src/main/java/cl/bibliotecaam/empleado/empleado/assembler/EmpleadoModelAssembler.java
@@ -0,0 +1,20 @@
+package cl.bibliotecaam.empleado.empleado.assembler;
+
+import cl.bibliotecaam.empleado.empleado.controller.EmpleadoController;
+import cl.bibliotecaam.empleado.empleado.dto.EmpleadoResponseDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class EmpleadoModelAssembler implements RepresentationModelAssembler<EmpleadoResponseDTO, EntityModel<EmpleadoResponseDTO>> {
+
+    @Override
+    public EntityModel<EmpleadoResponseDTO> toModel(EmpleadoResponseDTO empleadoDto){
+        return EntityModel.of(empleadoDto,
+                linkTo(methodOn(EmpleadoController.class).obtenerPorId(empleadoDto.getId_emp())).withSelfRel(),
+                linkTo(methodOn(EmpleadoController.class).obtenerTodos()).withRel("empleados"));
+    }
+}
