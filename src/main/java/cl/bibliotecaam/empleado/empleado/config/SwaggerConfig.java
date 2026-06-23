aa12fcc50cb302b6ff7326017c733bd8abfd54f0
MAGAQ <panamasado41@gmail.com>
2026-06-23 05:29:39 -0400

    Probando e instalando Swagger

---
+++ src/main/java/cl/bibliotecaam/empleado/empleado/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package cl.bibliotecaam.empleado.empleado.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+        return new OpenAPI()
+                .info(new Info().title("API 2026 - Empleados de la Biblioteca AM")
+                        .version("1.0")
+                        .description("Documentacion de la API para el registro de empleados de la Biblioteca AM"))
+                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .addSecurityItem(securityRequirement);
+    }
+}
